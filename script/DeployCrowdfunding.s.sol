4cf7126f2ce8a65c9c33ab14947695c279addc80
Abdul100-han <abdulmuhd857@gmail.com>
2026-06-01 11:13:43 +0100

    change campaign contract to 60 days

--- script/DeployCrowdfunding.s.sol
+++ script/DeployCrowdfunding.s.sol
@@ -16,12 +16,13 @@ contract DeployCrowdfunding is Script {
     /// @notice Funding target expressed in USD with 18 decimals.
     uint256 private constant FUNDING_TARGET_USD = 1_000e18;
 
-    /// @notice Campaign duration expressed in seconds for a 5-day campaign.
-    uint256 private constant CAMPAIGN_DURATION = 5 days;
+    /// @notice Campaign duration expressed in seconds for a 60-day campaign.
+    uint256 private constant CAMPAIGN_DURATION = 60 days;
 
     /// @notice Chainlink Sepolia ETH/USD price feed.
     address private constant SEPOLIA_ETH_USD_PRICE_FEED =
-        0x694aa18559F431442F925085d7994A010305BA10;
+            0x694AA1769357215DE4FAC081bf1f309aDC325306;
+        // 0x694aa18559F431442F925085d7994A010305BA10;
 
     /**
      * @notice Broadcasts the deployment transaction and returns the deployed contract address.
